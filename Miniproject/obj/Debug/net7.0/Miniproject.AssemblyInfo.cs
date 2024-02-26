2a492e3e587d95bb0b58ecc2bde34af95bc170d7
TigranesTG <job.limbeek@gmail.com>
2024-02-26 13:53:36 +0100

    - first

--- Miniproject/obj/Debug/net7.0/Miniproject.AssemblyInfo.cs
+++ Miniproject/obj/Debug/net7.0/Miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba7d106a3f71c0fd21e44a5f2c64558eb25d685")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
