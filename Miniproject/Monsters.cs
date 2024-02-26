513e1f4b9441bbac0437db9c8db3526e61c8aa60
TigranesTG <job.limbeek@gmail.com>
2024-02-26 15:57:49 +0100

    - inits voor classes

--- Miniproject/Monsters.cs
+++ Miniproject/Monsters.cs
@@ -1,1 +1,17 @@
-// test
+class Monster
+{
+    public int ID;
+    public string name;
+    public int damage;
+    public int currenthp;
+    public int maximumhp;
+
+    public Monster(int monster_id, string monster_name, int monster_damage, int CurrentHP, int MaximumHP)
+    {
+        ID = monster_id;
+        name = monster_name;
+        damage = monster_damage;
+        currenthp = CurrentHP;
+        maximumhp = MaximumHP;
+    }
+}
