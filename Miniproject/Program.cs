29d2558fb12473f3da71adbccc2daef91d062888
Job Limbeek <Job.Limbeek@gmail.com>
2024-02-27 21:51:19 +0100

    - more program.cs

--- Miniproject/Program.cs
+++ Miniproject/Program.cs
@@ -12,12 +12,12 @@
             Console.WriteLine("S: See game stats");
             Console.WriteLine("M: Move");
             Console.WriteLine("F: Fight");
-            Console.WriteLine("Q: Quit\n");
+            Console.WriteLine("Q: Quit");
             string? choice = Console.ReadLine();
 
             if (choice.ToUpper() == "S")
             {
-                Console.WriteLine($"Your current weapon is the {mc.weapon}");
+                Console.WriteLine($"\nYour current weapon is the {mc.weapon}");
                 Console.WriteLine($"You are at {mc.currenthp}/{mc.maximumhp}hp");
                 Console.WriteLine("Press I to see inventory");
                 string? choice2 = Console.ReadLine();
@@ -26,7 +26,7 @@
                     // show weapons
                     // show healing potions
                     // show other items
-                    Console.WriteLine("Press H to use a healing potion to regain 5hp");
+                    Console.WriteLine("\nPress H to use a healing potion to regain 5hp");
                     string? choice3 = Console.ReadLine();
                     if (choice3.ToUpper() = "H")
                     {
@@ -36,7 +36,7 @@
             }
             else if (choice.ToUpper() == "M")
             {
-                Console.WriteLine("Where would you like to go?");
+                Console.WriteLine("\nWhere would you like to go?");
                 Console.WriteLine($"You are at: {mc.location}. From here you van go:");
                 Console.WriteLine();
             }
